62134dedfb77691f8a2bd055db97850d6142dc61
Manuprasad384 <86980791+Manuprasad384@users.noreply.github.com>
2023-06-30 13:04:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+{AOS.init()}
+
+
+function openForm() {
+  document.getElementById("form").style.display = "block";
+  document.getElementById("btn").style.display = "none";
+}
+
+function cancel() {
+document.getElementById("form").style.display = "none";
+document.getElementById("btn").style.display = "block";
+}
+
+
+
