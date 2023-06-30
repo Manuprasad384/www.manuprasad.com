5f0336564a2918d2d4c9387a2c5f10ef078eb171
Manuprasad384 <86980791+Manuprasad384@users.noreply.github.com>
2023-06-30 18:16:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,5 +20,17 @@ document.getElementById("form").style.display = "none";
 document.getElementById("btn").style.display = "block";
 document.getElementById("img").style.display = "block";
 
+  $.ajax({
+      url:'https://script.google.com/macros/s/AKfycbz9NDWcBZGV-hpN4EWVgLe3HC8XW0InNV4MdruRdVDTfc8282hv5kIudW2p_7pu9FAk7A/exec',
+      method:'post',
+
+      data:$('#myForm').serialize(),
+      success:(res)=>{
+      document.location.reload();
+       
+        document.getElementById("myForm").reset();
+      },
+      error:(err)=>{alert('somthing error')}
+    })
 
 }
