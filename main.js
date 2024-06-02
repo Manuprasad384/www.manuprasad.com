5d79f124a5c84f0e41c33327a50beed3d15ab016
Manuprasad Mp <86980791+Manuprasad384@users.noreply.github.com>
2024-06-02 11:49:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,180 @@
+{AOS.init()}
+
+var b=0;
+function openForm() {
+
+if(b==1){
+document.getElementById("btm1").style.animation="menu4 0.2s 1 ";
+document.getElementById("btm2").style.animation="menu5 0.2s 1";
+document.getElementById("btm3").style.animation="menu6 0.2s 1";
+document.getElementById("form-popup").style.display = "block";
+
+  document.getElementById("form-popup").style.animation="popup 1s 0.5";
+  a=0;
+setTimeout(function(){
+document.getElementById("btm1").style.display="none";
+document.getElementById("btm2").style.display="none";
+document.getElementById("btm3").style.display="none";
+
+document.getElementById("mnb").src="Menu.png";
+
+},200);}else{
+
+  document.getElementById("form-popup").style.display = "block";
+  document.getElementById("form-popup").style.animation="popup 1s 0.5";}
+}
+
+function cancel() {
+document.getElementById("form-popup").style.animation="popdown 1s 1";
+setTimeout(function(){
+document.getElementById("form-popup").style.display = "none";},1000);
+
+
+}
+
+
+
+function send() {
+
+
+
+  document.getElementById("form-popup").style.display = "none";
+  
+    $.ajax({
+      url:'https://script.google.com/macros/s/AKfycbz9NDWcBZGV-hpN4EWVgLe3HC8XW0InNV4MdruRdVDTfc8282hv5kIudW2p_7pu9FAk7A/exec',
+      method:'post',
+
+      data:$('#form-container').serialize(),
+      success:(res)=>{
+      
+      document.location.reload();
+       
+        document.getElementById("form-container").reset();
+      },
+      error:(err)=>{alert('somthing error')}
+    })
+
+
+}
+
+
+function myfunction(){
+
+setTimeout(function(){
+    
+
+    document.getElementById("btnmenu").style.display="block";
+    
+    },3000);
+}
+
+var a=0;
+function menu(){
+t1=0;t2=0;t3=0;
+if(a == 0){
+cancel();
+document.getElementById("btm1").style.animation="menu1 0.2s 1";
+document.getElementById("btm2").style.animation="menu2 0.2s 1";
+document.getElementById("btm3").style.animation="menu3 0.2s 1";
+document.getElementById("btm1").style.display="block";
+document.getElementById("btm2").style.display="block";
+document.getElementById("btm3").style.display="block";
+a=1;b=1;
+document.getElementById("mnb").src="Cbtn.png";
+
+}
+else{
+    
+document.getElementById("btm1").style.animation="menu4 0.2s 1 ";
+document.getElementById("btm2").style.animation="menu5 0.2s 1";
+document.getElementById("btm3").style.animation="menu6 0.2s 1";
+
+setTimeout(function(){
+document.getElementById("btm1").style.display="none";
+document.getElementById("btm2").style.display="none";
+document.getElementById("btm3").style.display="none";
+document.getElementById("mnb").src="Menu.png";
+
+},200);
+a=0;
+}
+
+    
+}
+
+function project(){
+    document.location=
+    " https://manuprasad384.github.io/Calculator/";
+}
+
+
+function bio(){
+   window.alert("NOT SET YET");
+    
+}
+function home(){
+alert("NOT SET YET");
+    
+}
+
+
+
+var t1=0,t2=0,t3=0;
+function touch1(event){
+var y=event.touches[0].clientX;
+   
+    if(y > 0){
+    document.getElementById("btm1").style.animation="touch1 0.2s 1";
+    
+    setTimeout(function(){
+    document.getElementById("btm1").style.display="none";
+        },150);t1=1;
+        
+        if(t1 > 0 && t2 > 0 && t3 > 0 ){
+            document.getElementById("mnb").src="Menu.png";
+            t1=0;t2=0;t3=0;a=0;
+        }
+   
+        }
+    
+}
+
+
+
+function touch2(event){
+var y=event.touches[0].clientX;
+
+     if(y > 0){
+    document.getElementById("btm2").style.animation="touch2 0.2s 1";
+    
+    setTimeout(function(){
+    document.getElementById("btm2").style.display="none";
+         },150);t2=1;
+    if(t1 > 0 && t2 > 0 && t3 > 0 ){
+            document.getElementById("mnb").src="Menu.png";
+            t1=0;t2=0;t3=0;a=0;
+        }
+        }
+    
+}
+
+
+
+function touch3(event){
+var y=event.touches[0].clientX;
+
+     if(y > 0){
+     document.getElementById("btm3").style.animation="touch3 0.2s 1";
+    
+    setTimeout(function(){
+    document.getElementById("btm3").style.display="none";
+         },150);t3=1;
+    
+        if(t1 > 0 && t2 > 0 && t3 > 0 ){
+            document.getElementById("mnb").src="Menu.png";
+            t1=0;t2=0;t3=0;a=0;
+        }
+        }
+    
+}
+
